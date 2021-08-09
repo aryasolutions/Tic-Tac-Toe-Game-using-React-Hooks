9a175e137399550ca6fc323bedd237dda380a1ff
aryasolutions <aryaitsol@gmail.com>
2021-08-09 22:40:13 +0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,88 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import SquareComponent from "./modules/SquareComponent";
+import swal from 'sweetalert';
 
-const clearState = ["", "", "", "", "", "", "", "", "", ""];
+
+
+const clearState = ["", "", "", "", "", "", "", "",];
+
+
 
 function App() {
     const [gameState, updateGameState] = useState(clearState)
     const [isXChance, updateIsXChance] = useState(false)
+    const [Reset, updateReset] = useState(true)
+    const [XName, updateXName] = useState("")
+    const [OName, updateOName] = useState("")
+
 
     const onUserClicked = (index) => {
         let strings = Array.from(gameState);
         if (strings[index])
             return;
         strings[index] = isXChance ? "X" : "0";
-        updateIsXChance(!isXChance)
-        updateGameState(strings)
+        updateIsXChance(!isXChance);
+        updateGameState(strings);
     }
 
     const clearGame = () => {
-        updateGameState(clearState)
+        updateGameState(clearState);
+        updateReset(!Reset);
+        updateOName("")
     }
+    useEffect(() => {
+        swal({
+            title: 'Are you sure want to start New \n \n\nTic Tac Toe Game?',
+            // buttons: ["Yes", "No"],
+            buttons: {
+                // cancel: "No",
+                No : true,
+                Yes: true,
+              },
+        }).then((value) => {
+            switch (value) {
+                case "Yes":
+                    swal("Enter X Player Name: ", {
+                        content: "input",
+                    }).then((Xresult) => {
+                        if (Xresult) {
+                            updateXName(Xresult);
+                            console.log("XResult: " + XName);
+                        }
+                    }).then(() => {
+                        swal("Enter X Player Name: ", {
+                            content: "input",
+                        }).then((Oresult) => {
+                            if (Oresult) {
+                                updateOName(Oresult);
+                                console.log("Oresult: " + OName);
+                            }
+                        })
+                    })
+                  break;
+             
+                case "No":
+                //   swal("Gotcha!", "Pikachu was caught!", "success");
+                  break;
+             
+              }
+        });
+
+    }, [Reset])
+
+
     useEffect(() => {
         let winner = checkWinner();
         if (winner) {
-            clearGame();
-            alert(`Ta da ! ${winner} won the Game !`)
+            console.log(winner, XName, OName)
+            if (winner === 'X') {
+                swal("Good job!", ` ${XName} won the Game !`, "success").then(()=>{clearGame()});
+            }
+            if (winner === '0') {
+                swal("Good job!", ` ${OName} won the Game !`, "success").then(()=>{clearGame()});
+            }
         }
     }, [gameState])
-
     const checkWinner = () => {
         const lines = [
             [0, 1, 2],
@@ -49,26 +105,29 @@ function App() {
     }
 
     return (
-        <div className="app-header">
-            <p className="heading-text">React Tic Tac Toe - 2020</p>
-            <p className="fc-aqua fw-600">Aray Solution</p>
+        <div class="frist">
+        <div className={(OName ? "app-header" : "Hide")} >
+            <p className="heading-text">React Tic Tac Toe - 2020</p> 
+            <p className="fc-aqua fw-600"> X player is {XName} and 0 Player is {OName}</p>
             <div className="row jc-center">
-                <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(0)} state={gameState[0]}/>
-                <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(1)} state={gameState[1]}/>
-                <SquareComponent className="b-bottom" onClick={() => onUserClicked(2)} state={gameState[2]}/>
+                <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(0)} state={gameState[0]} />
+                <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(1)} state={gameState[1]} />
+                <SquareComponent className="b-bottom" onClick={() => onUserClicked(2)} state={gameState[2]} />
             </div>
             <div className="row jc-center">
-                <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(3)} state={gameState[3]}/>
-                <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(4)} state={gameState[4]}/>
-                <SquareComponent className="b-bottom" onClick={() => onUserClicked(5)} state={gameState[5]}/>
+                <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(3)} state={gameState[3]} />
+                <SquareComponent className="b-bottom-right" onClick={() => onUserClicked(4)} state={gameState[4]} />
+                <SquareComponent className="b-bottom" onClick={() => onUserClicked(5)} state={gameState[5]} />
             </div>
             <div className="row jc-center">
-                <SquareComponent className="b-right" onClick={() => onUserClicked(6)} state={gameState[6]}/>
-                <SquareComponent className="b-right" onClick={() => onUserClicked(7)} state={gameState[7]}/>
-                <SquareComponent onClick={() => onUserClicked(8)} state={gameState[8]}/>
+                <SquareComponent className="b-right" onClick={() => onUserClicked(6)} state={gameState[6]} />
+                <SquareComponent className="b-right" onClick={() => onUserClicked(7)} state={gameState[7]} />
+                <SquareComponent onClick={() => onUserClicked(8)} state={gameState[8]} />
             </div>
             <button className="clear-button" onClick={clearGame}>Clear Game</button>
         </div>
+        </div>
+
     );
 }
 
